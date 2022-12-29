15883ce7c60f9a7b31c57e8e4f3da61d30ea29d7
Jamal Jones <74991730+OfficialJamalJones@users.noreply.github.com>
2022-12-29 17:38:31 -0500

    Add files via upload

---
+++ APINetworking/APINetworking/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  APINetworking
+//
+//  Created by Consultant on 12/29/22.
+//
+
+//HW-1: Use any open source api and utilize it in your app, use URLSession networking to fetch data and parse it and show this in your app. Use Swift and MVVM for this tasks.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Testing")
+    }
+
+
+}
+
